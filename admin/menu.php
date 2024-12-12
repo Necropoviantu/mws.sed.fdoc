e59145eb49798a3626c5c192b5e8f67e0a8b0652
necropoviantu <mordvinov.sv@gmail.com>
2024-12-12 17:57:40 +0700

    init new project

---
+++ admin/menu.php
@@ -0,0 +1,17 @@
+<?php
+defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadLanguageFile(__FILE__);
+
+$aMenu[] = [
+    "parent_menu" => "global_menu_custom",
+    "sort" => 1900,
+    "text" => Loc::getMessage('MWS_DEAL_ENTITY_MODULE_MENU'),
+    "title" => Loc::getMessage('MWS_DEAL_ENTITY_MODULE_MENU_TITLE'),
+    "url" => BX_ROOT . '/admin/mwsdealentitysettings.php?lang=' . LANGUAGE_ID,
+    "icon" => "util_menu_icon",
+    "page_icon" => "util_page_icon"
+];
+
+return $aMenu;
